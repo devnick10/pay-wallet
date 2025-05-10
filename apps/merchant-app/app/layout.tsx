dcc6a9d4078d40446a2321e34e8a9c86c5710b57
devnick10 <nickbhoyar683@gmail.com>
2025-05-10 11:15:24 +0000

    chore:fix type issue build error

--- apps/merchant-app/app/layout.tsx
+++ apps/merchant-app/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Providers } from "../providers";
-import { ThemeProvider } from "next-themes";
+import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- apps/merchant-app/app/lib/authOptions.ts
+++ apps/merchant-app/app/lib/authOptions.ts
@@ -1,20 +1,73 @@
+// import prisma from "@repo/db/client";
+// import { Account, User } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID || "",
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+//     })
+//   ],
+//   secret: process.env.JWT_SECRET,
+//   pages: {
+//     signIn: '/signin'
+//   },
+//   callbacks: {
+//     async signIn({ user, account }: { user: User, account: Account }) {
+//       if (account?.provider === "google") {
+//         try {
+//           if (!user.email) {
+//             console.error("No email found in user object");
+//             return false;
+//           }
+
+//           const existingUser = await prisma.merchant.findUnique({
+//             where: {
+//               email: user.email
+//             }
+//           });
+
+//           if (!existingUser) {
+//             await prisma.merchant.create({
+//               data: {
+//                 email: user.email,
+//                 auth_type: "Google",
+//                 name: user.name || "",
+//               }
+//             });
+//           }
+//         } catch (error) {
+//           console.error("Error during Google sign-in:", error);
+//           return false;
+//         }
+//       }
+//       return true;
+//     }
+//   },
+
+// }
+
 import prisma from "@repo/db/client";
-import { Account, User } from "next-auth";
+import { NextAuthOptions, Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
     })
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/signin'
   },
   callbacks: {
-    async signIn({ user, account }: { user: User, account: Account }) {
+    async signIn(params) {
+      const { user, account } = params;
+
       if (account?.provider === "google") {
         try {
           if (!user.email) {
@@ -23,9 +76,7 @@ export const authOptions = {
           }
 
           const existingUser = await prisma.merchant.findUnique({
-            where: {
-              email: user.email
-            }
+            where: { email: user.email }
           });
 
           if (!existingUser) {
@@ -37,13 +88,25 @@ export const authOptions = {
               }
             });
           }
+          return true;
         } catch (error) {
           console.error("Error during Google sign-in:", error);
           return false;
         }
       }
       return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: Session, token: JWT }) {
+      if (session.user) {
+        session.user = token.user || "";
+      }
+      return session;
     }
-  },
-
-}
+  }
+};
