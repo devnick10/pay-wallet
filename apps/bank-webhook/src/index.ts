fb11fa847b442dba74f03ffb4230ba87199ea441
devnick10 <nickbhoyar683@gmail.com>
2025-06-01 19:56:49 +0000

    implement payout dummy api

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -66,7 +66,69 @@ app.post("/hdfcWebhook", async (req, res) => {
             message: "Error while processing webhook"
         })
     }
+})
 
+app.post("/payout/hdfcWebhook", async (req, res) => {
+    const result = onRampValidationSchema.safeParse(req.body)
+    if (!result.success) {
+        res.status(411).json({
+            message: "Verify inputs"
+        })
+        return
+    }
+
+    const {
+        token, amount, user_identifier
+    } = result.data;
+
+    const payoutTransactionStatus = await db.payout.findUnique({
+        where: {
+            token
+        }
+    })
+
+    if (payoutTransactionStatus?.status == "Success") {
+        res.json({
+            message: "Invalid token"
+
+        })
+        return
+    }
+
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    merchantId: Number(user_identifier)
+                },
+                data: {
+                    locked: {
+                        decrement: Number(amount)
+                    },
+                    amount: {
+                        decrement: Number(amount)
+                    }
+                }
+            }),
+            db.payout.updateMany({
+                where: {
+                    token
+                },
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch (e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
 })
 
 app.listen(3003);
