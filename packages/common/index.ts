519b0d63ba948fbb44b96f2b1dbf0b167147780c
devnick10 <nickbhoyar683@gmail.com>
2026-04-07 22:04:02 +0530

    chore: add zod error message

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,12 +1,13 @@
 import z from "zod";
+
 const signInSchema = z.object({
-  name: z.string().min(3, { message: "Name should be least 4 character" }),
+  name: z.string({ message: "Name is required" }).min(3, { message: "Name should be least 4 character" }),
   phone: z
-    .string()
+    .string({ message: "Phone number is required" })
     .min(10, { message: "Invalied length on number" })
     .max(10, { message: "Invalied length on number" }),
   password: z
-    .string()
+    .string({ message: "Password is required" })
     .min(3, { message: "Password should be least 4 character" }),
 });
 
@@ -17,6 +18,20 @@ const onRampValidationSchema = z.object({
     .string({ message: "Amount required" })
     .min(2, { message: "Provied valid minimum amount" }),
 });
-export type CredentialsType = z.infer<typeof signInSchema>;
 
-export { signInSchema, onRampValidationSchema };
+const SUPPORTED_BANK_PROVIDERS = ["HDFC Bank", "Axis Bank"] as const;
+
+const onRampTransactionSchema = z.object({
+  amount: z.number({ message: "Amount is required" }),
+  provider: z.enum(SUPPORTED_BANK_PROVIDERS),
+});
+
+const p2pTransferSchema = z.object({
+  phone: z.number({ message: "Phone number is required" }),
+  amount: z.number({ message: "Amount is required" }),
+});
+
+
+export type CredentialsType = z.infer<typeof signInSchema>;
+export { onRampTransactionSchema, onRampValidationSchema, p2pTransferSchema, signInSchema };
+
