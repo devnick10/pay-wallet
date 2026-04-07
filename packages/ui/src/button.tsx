f1b30ae794fe4e8e83cf7aa53b5487fd95ea3ce8
devnick10 <nickbhoyar683@gmail.com>
2026-04-07 22:04:19 +0530

    chore: fix lint

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,7 +11,7 @@ interface ButtonProps {
 export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
     <button
-      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5"
+      className={`text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 ${className}`}
       onClick={onClick}
     >
       {children}
